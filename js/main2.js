bc1081fbb9b65d5cf009bcada97c9226decab032
RalphLiao <38518874a@gmail.com>
2024-06-18 02:12:35 +0100

    優化排版

--- js/main2.js
+++ js/main2.js
@@ -1,13 +1,13 @@
 lightbox.option({
-    'resizeDuration': 200,
-    'wrapAround': true,
-    'albumLabel': "Image %1 of %2"
-  })
+  resizeDuration: 200,
+  wrapAround: true,
+  albumLabel: "Image %1 of %2",
+});
 
 function openModal() {
-    document.getElementById('myModal').style.display = "block";
+  document.getElementById("myModal").style.display = "block";
 }
 
 function closeModal() {
-    document.getElementById('myModal').style.display = "none";
+  document.getElementById("myModal").style.display = "none";
 }
